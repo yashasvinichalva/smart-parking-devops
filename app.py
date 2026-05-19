242a7145e1a5dcf01b3d21abedfa514d6d94b465
suresh sethi <yashasvinichalva@gmail.com>
2026-05-19 20:12:28 +0530

    Added Docker support and fixed Flask host binding

--- app.py
+++ app.py
@@ -482,4 +482,4 @@ def api_slots():
 
 if __name__ == '__main__':
     init_db()        # create tables + seed data on first run
-    app.run(debug=True, port=5000)
+    app.run(host="0.0.0.0", port=5000, debug=True)
